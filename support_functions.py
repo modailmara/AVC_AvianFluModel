3d985400ccdd2a39073a3bc54bdc86b3c94ecc55
modailmara <gregor@modailmara.ca>
2025-07-29 11:15:25 -0300

    Made the method to find the project path a little more resilient to different paths.

--- support_functions.py
+++ support_functions.py
@@ -12,18 +12,19 @@ def get_root_dir():
     """
     cwd = Path.cwd()
 
-    try:
-        # find where WeCress is in the path (this is where the ValueError will be raised if it isn't)
-        wecress_index = cwd.parts.index('AvianFlu')
+    # find the last part where AvianFlu is a substring
+    last_index = None
+    for i, part in enumerate(cwd.parts):
+        if 'avianflu' in part.lower():
+            last_index = i
 
-        # new path just up to the WeCress bit
-        wecress_path = Path(*cwd.parts[:wecress_index+1])
+    if last_index is None:
+        # we're not in a subdirectory of AvianFlu (how?)
+        raise ValueError("{} is not a subdirectory of AvianFlu\n{}".format(cwd))
+    else:
+        avianflu_path = Path(*cwd.parts[:last_index+1])
 
-    except ValueError as e:
-        # we're not in a subdirectory of WeCress (how?)
-        raise ValueError("{} is not a subdirectory of AvianFlu\n{}".format(cwd, e))
-
-    return wecress_path
+    return avianflu_path
 
 
 def get_input_data_dir():
